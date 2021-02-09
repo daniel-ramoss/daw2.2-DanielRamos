0489e73d6300980029566549f191f257acc6450e
daniel-ramoss <72920937+daniel-ramoss@users.noreply.github.com>
2021-02-09 15:23:56 +0100

    300_AgendaAJAX -9-2-2021
    No he conseguido hacer la parte que se pedía, he tenido que solucionar otros problemas que habían surgido con la misma práctica y me ha llevado más tiempo del que debía.

---
+++ 300_AgendaAJAX/_com/Clases.php
@@ -0,0 +1,78 @@
+<?php
+
+abstract class Dato
+{
+}
+
+trait Identificable
+{
+    protected  $id;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+}
+
+class Categoria extends Dato implements JsonSerializable
+{
+    use Identificable;
+
+    private  $nombre;
+    private  $personasPertenecientes;
+
+    public function __construct(int $id, string $nombre)
+    {
+        $this->setId($id);
+        $this->setNombre($nombre);
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            "nombre" => $this->nombre,
+            "id" => $this->id,
+        ];
+
+    }
+
+    public function getNombre(): string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function obtenerPersonasPertenecientes(): array
+    {
+        if ($this->personasPertenecientes == null) $personasPertenecientes = DAO::personaObtenerPorCategoria($this->id);
+
+        return $personasPertenecientes;
+    }
+}
+
+class Persona extends Dato
+{
+    use Identificable;
+
+    private  $nombre;
+    private  $apellidos;
+    // ...
+    private  $categoriaId;
+    private  $categoria;
+
+    public function obtenerCategoria(): Categoria
+    {
+        if ($this->categoria == null) $categoria = DAO::categoriaObtenerPorId($this->categoriaId);
+
+        return $categoria;
+    }
+}
